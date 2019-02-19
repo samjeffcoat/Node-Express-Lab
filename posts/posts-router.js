93c8e730615db2b96f4af5c9c95743a47249049f
samjeffcoat <samjeffcoat@gmail.com>
2019-02-19 17:06:19 -0600

    Adds the get method and searches for id and returns. Successfully returns post with that Id. If it didnt have length, then it wouldnt work

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -33,4 +33,22 @@ router.post("/", async (req, res) => {
     }
 })
 
+router.get("/:id", async (req, res) => {
+    try{
+        const post = await db.findById(req.params.id);
+        if (post.length > 0) {
+            res.status(200).json(post);
+        }
+        else{
+            res.status(404).json({ message: "The post with the specified ID does not exist"})
+        }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: "The post information could not be retrieved"});
+    }
+});
+
+
+
+
 module.exports = router
