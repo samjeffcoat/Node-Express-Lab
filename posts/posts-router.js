b5731f7380e1b8357db4ef84dd5474fec6e98c8c
samjeffcoat <samjeffcoat@gmail.com>
2019-02-19 16:50:44 -0600

    Adds the post method

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -13,37 +13,24 @@ router.get('/',  async (req, res) => {
     res.status(500).json({error: "The posts information could not be retrieved."});
 }
 })
+// Creates a post and check to make sure the post has a title and contents or it gives an error. Sets postFull requirements and assigns variables. Verified in postman.
 
-/// POST	/api/posts	Creates a post using the information sent inside the request body.
-////When the client makes a POST request to /api/posts:
-//// If the request body is missing the title or contents property:
-//// A cancel the request.
-//// B respond with HTTP status code 400 (Bad Request).
-//// C return the following JSON response: { errorMessage: "Please provide title and contents for the post." }.
-
-////2 If the information about the post is valid:
-////A save the new post the the database.
-////B return HTTP status code 201 (Created).
-////C return the newly created post.
-
-////3 If there's an error while saving the user:
-////A cancel the request.
-////B respond with HTTP status code 500 (Server Error).
-////C return the following JSON object: { error: "There was an error while saving the post to the database" }.
-
-router.post("/api/posts", (req, res) => {
-    let post = req.body
-    if (!post.title || !post.contents) {
+router.post("/", async (req, res) => {
+    if (!req.body.title || !req.body.contents) {
         res.status(400).json({ error: "Please provide title and contents for the post." });
         return;
+    } 
+    try {
+        const postFull = {
+            title: req.body.title,
+            contents: req.body.contents
+        };
+        let newId= await db.insert(postFull);
+        let newPost = await db.findById(newId.id);
+        res.status(201).json(newPost);
+    } catch(error){
+        res.status(500).json({error: "There was an error while saving the post to the database"});
     }
-    db.insert(post)
-        .then(post => {
-            res.status(201).json({ success: true, post })
-        })
-        .catch(err => {
-            res.status(500).json({ err: "there was an error while saving the post to the database!" })
-        })
 })
 
 module.exports = router
